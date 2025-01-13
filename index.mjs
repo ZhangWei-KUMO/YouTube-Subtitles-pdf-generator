75ddef321421a7f7a279b709128b32017bd077f0
ZhangWei-KUMO <kanseefoil@gmail.com>
2025-01-13 15:19:34 +0800

    support multi langueges

--- index.mjs
+++ index.mjs
@@ -5,7 +5,7 @@ import fetch from 'node-fetch';
 
 const app = express();
 const port = 3000;
-const MAX_GOOGLE_TRANSLATE_LENGTH = 10000;
+const MAX_GOOGLE_TRANSLATE_LENGTH = 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,24 +13,24 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 async function downloadYouTubeVideo(youtubeUrl) {
- try {
-     let res = await ytdl.getBasicInfo(youtubeUrl)
-     let videoDetails = res.videoDetails
-     let videoTitle = videoDetails.title
-     let videoAuthor = videoDetails.author.name
-     let {formats} = await ytdl.getInfo(youtubeUrl);
-     formats = formats.filter(format => format.hasAudio===true && format.hasVideo===true);
-     if(formats.length===0){
-     return null
-     }else{
-     let video = formats[0];
-     video.title = videoTitle;
-     video.author = videoAuthor;
-     return video
-     }
- }catch(e){
- console.error("获取视频信息失败", e)
- }
+    try {
+        let res = await ytdl.getBasicInfo(youtubeUrl)
+        let videoDetails = res.videoDetails
+        let videoTitle = videoDetails.title
+        let videoAuthor = videoDetails.author.name
+        let {formats} = await ytdl.getInfo(youtubeUrl);
+        formats = formats.filter(format => format.hasAudio===true && format.hasVideo===true);
+        if(formats.length===0){
+            return null
+        }else{
+            let video = formats[0];
+            video.title = videoTitle;
+            video.author = videoAuthor;
+            return video
+        }
+    }catch(e){
+        console.error("获取视频信息失败", e)
+    }
 }
 
 function formatTime(seconds) {
@@ -42,13 +42,13 @@ function formatTime(seconds) {
 
 function decodeHTMLEntities(text) {
     const entities = {
-    '&': '&',
-    '<': '<',
-    '>': '>',
-    '"': '"',
-    "'": "'",
-    "'": "'",
-    '': '',
+        '&': '&',
+        '<': '<',
+        '>': '>',
+        '"': '"',
+        "'": "'",
+        "'": "'",
+        '': '',
     };
     return text.replace(/&|<|>|"|'|'|`/g, match => entities[match]);
 }
@@ -73,9 +73,9 @@ async function convertXmlToSrt(xmlContent) {
                 const end = start + duration;
                 const formattedStartTime = formatTime(start);
                 const formattedEndTime = formatTime(end);
-                let englishText = cue._ ? decodeHTMLEntities(cue._).replace(/\n/g, ' ') : ""; 
-                // 处理&#39;
-                englishText = englishText.replace(/&#39;/g, "'");
+                let englishText = cue._ ? decodeHTMLEntities(cue._).replace(/\n/g, ' ') : "";
+                // 处理'
+                englishText = englishText.replace(/'/g, "'");
                 srtContent += `${index}\n${formattedStartTime} --> ${formattedEndTime}\n${englishText.trim()}\n\n`;
                 index++;
             });
@@ -85,9 +85,7 @@ async function convertXmlToSrt(xmlContent) {
 }
 
 
-async function requestGoogle(rawContent) {
-    const from = 'en';
-    const lang = 'zh-CN';
+async function requestGoogle(rawContent,from,lang) {
     if (from === null) {
         return { error: 'Unsupported source language.' };
     }
@@ -98,6 +96,8 @@ async function requestGoogle(rawContent) {
 
     try {
         const res = await fetch(googleTranslate + encodeURIComponent(rawContent));
+        // const res = await fetch(googleTranslate +rawContent);
+
         const data = await res.json();
         let str = '';
         data.sentences.forEach((element) => {
@@ -116,23 +116,15 @@ async function getVideoInfo(videoId) {
         const videoPageHtml = await videoPageResponse.text();
         const splittedHtml = videoPageHtml.split('"captions":');
         if (splittedHtml.length < 2) {
-            console.error("无法找到字幕信息");
-            return null;
+            return "There are no available subtitles tracks";
         }
         const { playerCaptionsTracklistRenderer } = JSON.parse(splittedHtml[1].split(',"videoDetails')[0].replace('\n', ''));
         const { captionTracks } = playerCaptionsTracklistRenderer;
         if (!captionTracks || captionTracks.length === 0) {
-            console.error("没有找到可用的英文字幕");
-            return null;
+            return "There are no available subtitles tracks";
         }
-
-        const englishSubTrack = captionTracks.find(track => track.languageCode === 'en');
-
-        if (!englishSubTrack) {
-            console.error("没有找到可用的英文字幕");
-            return null;
-        }
-        return englishSubTrack.baseUrl;
+  
+        return captionTracks[0].baseUrl;
     } catch (error) {
         console.error("获取视频信息出错:", error);
         return null;
@@ -141,18 +133,18 @@ async function getVideoInfo(videoId) {
 
 
 function escapeXML(str) {
-  return String(str)
-    .replace(/&/g, '&')
-    .replace(/</g, '<')
-    .replace(/>/g, '>')
-    .replace(/"/g, '"')
-    .replace(/'/g, '\'');
+    return String(str)
+        .replace(/&/g, '&')
+        .replace(/</g, '<')
+        .replace(/>/g, '>')
+        .replace(/"/g, '"')
+        .replace(/'/g, '\'');
 }
 
 function chunkString(str, size) {
     const numChunks = Math.ceil(str.length / size);
     const chunks = new Array(numChunks);
-    
+
     for (let i = 0, o = 0; i < numChunks; ++i, o += size) {
         chunks[i] = str.substr(o, size);
     }
@@ -161,54 +153,54 @@ function chunkString(str, size) {
 
 
 
-async function translateAndSaveVtt(youtubeUrl, res) {
+async function translateAndSaveVtt(youtubeUrl, from, lang, res) {
     const videoId = youtubeUrl.split('v=')[1];
     const vttUrl = await getVideoInfo(videoId);
     if (!vttUrl) {
+        console.log(from,lang)
         console.error("获取字幕链接失败");
-        res.render('index', { error: '获取字幕链接失败' });
+        res.json({success: false, error: "获取字幕链接失败"}); // 返回 JSON 错误
         return;
     }
 
     try {
         const response = await fetch(vttUrl);
-        // 获取到xml内容
         let vttContent = await response.text();
         let srtContent = await convertXmlToSrt(vttContent);
- 
-        const srtChunks = chunkString(srtContent, MAX_GOOGLE_TRANSLATE_LENGTH)
+
+        const srtChunks = chunkString(srtContent, MAX_GOOGLE_TRANSLATE_LENGTH);
 
         let translatedSrtContent = '';
-        for(const srtChunk of srtChunks){
-                // 分块翻译，得到结果
-             let translatedTextChunk = await requestGoogle(srtChunk)
-             translatedSrtContent += translatedTextChunk;
+        for (const srtChunk of srtChunks) {
+            let translatedTextChunk = await requestGoogle(srtChunk, from, lang);
+            if (translatedTextChunk.error) {
+                 res.json({ success: false, error: translatedTextChunk.error });
+                return;
+            }
+            translatedSrtContent += translatedTextChunk;
         }
-      
+
         const englishSrtLines = srtContent.trim().split('\n\n');
         const translatedLines = translatedSrtContent.trim().split('\n\n');
 
-         // 3. 初始化合并后的 SRT 内容字符串
         let mergedSrtContent = '';
-
-        // 4. 遍历英文字幕块，合并中文字幕
         for (let i = 0; i < englishSrtLines.length; i++) {
             const originalLine = englishSrtLines[i];
             const translatedLine = translatedLines[i];
             const originalParts = originalLine.split('\n');
-            const translatedPart = translatedLine ? translatedLine.split('\n')[2] : "";
+             const translatedPart = translatedLine ? translatedLine.split('\n')[2] : "";
             mergedSrtContent += `${originalParts[0]}\n${originalParts[1]}\n${originalParts[2]}${translatedPart ? `\n${translatedPart}` : ''}\n\n`;
         }
-        // 将合并后的 SRT 文件提供给前端浏览器下载
+        // 发送 SRT 文件供下载
         res.setHeader('Content-Disposition', `attachment; filename="${videoId}.srt"`);
         res.setHeader('Content-Type', 'text/plain');
         res.send(mergedSrtContent);
-        
     } catch (error) {
         console.error("处理字幕时出错:", error);
-        res.render('index', { error: '处理字幕时出错' });
+        res.json({success: false, error: "处理字幕时出错"}); // 返回 JSON 错误
     }
 }
+
 
 
 
@@ -223,38 +215,40 @@ app.get('/download', (req, res) => {
 
 app.post('/generate-subtitle', async (req, res) => {
     const youtubeUrl = req.body.youtubeUrl;
+    const subtitleLanguage = req.body.subtitleLanguage;
+    const originalLanguage = req.body.originalLanguage;
     if (!youtubeUrl) {
         res.render('index', { error: '请提供 YouTube 链接' });
         return;
     }
     try{
-        await translateAndSaveVtt(youtubeUrl, res);
+        await translateAndSaveVtt(youtubeUrl,originalLanguage,subtitleLanguage,res);
     }catch(e){
         console.error("出错", e)
-         res.render('index', { error: '出现错误, 请查看控制台' });
+        res.render('index', { error: '出现错误, 请查看控制台' });
     }
 });
 
 app.post('/generate-downloadlink', async (req, res) => {
     const youtubeUrl = req.body.youtubeUrl;
     if (!youtubeUrl) {
-       return res.json({ success: false, error: '请提供 YouTube 链接' });
+        return res.json({ success: false, error: '请提供 YouTube 链接' });
     }
     try{
         let video = await downloadYouTubeVideo(youtubeUrl);
-           if (video && video.url) {
-             // 添加 Content-Disposition 头，强制下载
+        if (video && video.url) {
+            // 添加 Content-Disposition 头，强制下载
             res.setHeader('Content-Disposition', `attachment; filename="${video.title}.${video.container}"`);
             res.json({ success: true, video: {
                 ...video,
-                 downloadUrl:video.url
+                downloadUrl:video.url
             }});
         }else{
             res.json({success: false, error: "获取视频信息失败"});
         }
     }catch(e){
         console.error("出错", e)
-       res.status(500).json({ success: false, error: '出现错误, 请查看控制台' });
+        res.status(500).json({ success: false, error: '出现错误, 请查看控制台' });
     }
 });
 
